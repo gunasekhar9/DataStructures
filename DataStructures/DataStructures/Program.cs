c78b56de8d5c72b4f070bee5f1857e4c0f64c3f0
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-20 19:26:35 +0530

    [gunasekhar9] add : DataStructures

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Problems");
+            LinkedList ln = new LinkedList();
+            ln.Add(56);
+            ln.Add(30);
+            ln.Add(70);
+            ln.Display();
+
+        }
+    }
+}
