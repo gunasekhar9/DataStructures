7082249033f603a4ac0d9e96368968264d95affd
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-21 16:35:46 +0530

    [gunasekhar9] add : Insert between Linked List is done

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -6,12 +6,10 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Problems");
             LinkedList ln = new LinkedList();
             ln.Add(56);
             ln.Add(30);
             ln.Add(70);
-            ln.InsertAtParticularPosition(2, 30);
             ln.Display();
         }
     }
